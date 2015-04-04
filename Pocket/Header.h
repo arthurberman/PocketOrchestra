5d608ea7aa97da22c2da0d64c2ad50f2c0d1b517
Arthur Berman <aberman@wr-130-64-221-230.medford.tufts.edu>
2015-04-04 14:54:05 -0400

    added podfile

---
+++ Pocket/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  Pocket
+//
+//  Created by Arthur Berman on 4/2/15.
+//  Copyright (c) 2015 Arthur Berman. All rights reserved.
+//
+
+#ifndef Pocket_Header_h
+#define Pocket_Header_h
+
+#import "PdBase.h"
+#import "PdBridge.h"
+
+#endif
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'Pocket' do
+    pod 'MIKMIDI', '~> 1.0'
+    pod 'libpd', '~> 0.7'
+end
+
+target 'PocketTests' do
+
+end
+
